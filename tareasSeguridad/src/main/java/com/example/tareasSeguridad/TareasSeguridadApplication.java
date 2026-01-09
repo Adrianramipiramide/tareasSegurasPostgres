24f31b8ecc6db281f0f36434e22ef9aac7573039
Adrian Rami Piramide <230053@fppiramide.com>
2026-01-09 10:53:49 +0100

    repo vaciao con dependencias

---
+++ tareasSeguridad/src/main/java/com/example/tareasSeguridad/TareasSeguridadApplication.java
@@ -0,0 +1,13 @@
+package com.example.tareasSeguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareasSeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareasSeguridadApplication.class, args);
+	}
+
+}
---
+++ tareasSeguridad/src/test/java/com/example/tareasSeguridad/TareasSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tareasSeguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
